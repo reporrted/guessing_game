fcd3b18413d77e355ad70e9390e9be419e0d6463
nick <spence.nick@gmail.com>
2022-06-22 16:53:53 -0400

    comitting to ch2

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,13 @@ use std::cmp::Ordering;
 use rand::Rng;
 
 fn main() {
-    println!("Guess the number!");
+    println!("Guess the Number!");
 
     let secret_number = rand::thread_rng().gen_range(1..101);
 
     println!("The secret number is: {}", secret_number);
 
-    println!("Please input your guess.");
+    println!("Please input your guess:");
 
     let mut guess = String::new();
 
@@ -17,12 +17,13 @@ fn main() {
         .read_line(&mut guess)
         .expect("Failed to read line");
 
+    let guess: u32 = guess.trim().parse().expect("Please type a number.");
+
     println!("You guessed: {}", guess);
 
     match guess.cmp(&secret_number) {
-        Ordering::Less => println("Too Small!"),
-        Ordering::Greater => println("Too Big!"),
-        Ordering::Equal => println("You Win!"),
+        Ordering::Less => println!("Too Small!"),
+        Ordering::Greater => println!("Too Big!"),
+        Ordering::Equal => println!("You Win!"),
     }
 }
-// TODO @reporrted 
